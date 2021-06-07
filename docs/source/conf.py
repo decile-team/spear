ad48bf52730aaff3eedcc80784276be4dbf9a3f0
ayushbits <ayush.hakmn@gmail.com>
2021-06-08 00:23:45 +0530

    changed for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../spear'))
 
 master_doc = 'index'
 autodoc_mock_imports = ["torch", "sklearn", "utils_jl", "models", "tensorflow", "snorkel", 
