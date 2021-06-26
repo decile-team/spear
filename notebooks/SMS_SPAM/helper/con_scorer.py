756d0834105f61b1612b1dd0cb8e47673b89563b
Dornavineeth <vineethdorna@gmail.com>
2021-06-26 21:37:25 +0530

    made minor changes in notebooks

--- notebooks/SMS_SPAM/helper/con_scorer.py
+++ notebooks/SMS_SPAM/helper/con_scorer.py
@@ -8,7 +8,11 @@ from gensim.models.keyedvectors import KeyedVectors
 import gensim.matutils as gm
 
 print("model loading")
-model = KeyedVectors.load_word2vec_format('data/SMS_SPAM/glove_w2v.txt', binary=False)
+model = None
+try:
+    model = KeyedVectors.load_word2vec_format('data/SMS_SPAM/glove_w2v.txt', binary=False)
+except:
+    model = KeyedVectors.load_word2vec_format('../../data/SMS_SPAM/glove_w2v.txt', binary=False)
 print("model loaded")
 
 def get_word_vectors(btw_words):
